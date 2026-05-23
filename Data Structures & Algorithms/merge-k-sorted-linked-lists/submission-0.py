5ef778ff8a4c3847451bdacb33e286d72b62c5e2
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2026-05-23 10:03:15 -0400

    Add: merge-k-sorted-linked-lists - submission-0

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-0.py
@@ -0,0 +1,80 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        # merging 2 for example
+        """
+            This requires to go through the list : O(k)
+            traverse k list at time
+        """
+        res = ListNode() #create new head
+        listIndex = 0
+        itemIndex = 0
+
+        # O(n) time complexity with n max element in array
+        def merge(l1:ListNode,l2: ListNode):
+            res = ListNode()
+            head = res
+            while l1 and l2:
+                # check which one is smaller
+                print(l1.val,l2.val,head.val)
+                if l1.val < l2.val:
+                    newNode = ListNode(l1.val);
+                    head.next = newNode
+                    head = newNode #make it the head
+                    l1 = l1.next
+                else:
+                    newNode = ListNode(l2.val);
+                    head.next = newNode
+                    head = newNode
+                    l2 = l2.next
+            # add remaining because one of them is bigger
+            while l1:
+                newNode = ListNode(l1.val);
+                head.next = newNode
+                head = newNode #make it the head
+                l1 = l1.next
+            while l2:
+                newNode = ListNode(l2.val);
+                head.next = newNode
+                head = newNode
+                l2 = l2.next
+            return res.next
+        
+        # we need to iterate in kth array
+        #  we can use devide and conquer where we split the lists into 2 halfgs and merge those
+
+        def mergeSort(lists):
+            if len(lists) == 0:
+                return None
+            if len(lists) == 1:
+                return lists[0]
+            # split in two
+            mid = len(lists) // 2
+            # recursively call 
+            left = mergeSort(lists[:mid])
+            right = mergeSort(lists[mid:])
+            return merge(left,right)
+        return mergeSort(lists)
+
+
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
