e4db7b2878033f1a2d6330e00856961d158bb741
Ntirandekura Tharcisse <119756165+tharcissentirandekura@users.noreply.github.com>
2026-05-23 10:44:28 -0400

    Add: merge-k-sorted-linked-lists - submission-4

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-4.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        """
+            The key idea is to merge two lists at a time which takes O(n) time and then return merged list head
+            now, we need to iterate through k lists and merge lists[i-1] and lists[i] and store results into lists[i]
+            at the end we just return the last list in the lists since it is the one with the final merged elements.
+        
+        """
+        if len(lists) == 0:
+            return None
+
+        for i in range(1,len(lists)):
+            lists[i] = self.merge(lists[i - 1], lists[i])
+        return lists[-1]
+        
+    # a helper to merge two lists
+    def merge(self,l1,l2):
+        dummy = ListNode()
+        tail = dummy
+        while l1 and l2:
+            # check which one is smaller
+            if l1.val < l2.val:
+                tail.next = l1
+                l1 = l1.next
+            else:
+                tail.next = l2
+                l2 = l2.next
+            tail = tail.next
+        # add remaining because one of them is bigger
+        if l1:
+            tail.next = l1
+        if l2:
+            tail.next = l2
+        return dummy.next
+    
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
